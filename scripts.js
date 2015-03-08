a6d5e931be507a3c5d7d99d15f00ae8b6eef6d93
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-03-08 17:08:11 -0400

    Created a variable called mappingDataArray that will equal the rows of the mappingData.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var mappingDataArray = mappingData.rows;
+
+for (var i=0; i<)
 var marker = L.marker([40.798306,-73.962713]).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup("<b>Home address</b><br>I live here.").openPopup();
