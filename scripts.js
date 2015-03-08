ecb89b445160395f7ef4049edae04b677384ef39
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-03-08 16:00:27 -0400

    Added marker to the map.

--- scripts.js
+++ scripts.js
@@ -4,3 +4,5 @@ var map = L.map('leafletMap').setView([40.714269,-74.005973], 13);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
