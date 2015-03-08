1a5fa91d93aba1f8a3d775bd3662aafd2962de93
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-03-08 15:58:33 -0400

    Added an open street map tile layer.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 var map = L.map('leafletMap').setView([40.714269,-74.005973], 13);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
